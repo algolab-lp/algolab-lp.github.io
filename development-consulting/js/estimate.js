c7a9501146f209208f1f774eb24dc352c5d465c0
rw-nue <wakabayashi@novatrade.co.jp>
2017-11-09 16:33:11 +0900

    refs #3 ワード設定javascript作成

---
+++ development-consulting/js/estimate.js
@@ -0,0 +1,57 @@
+$(function () {
+    var CATEGORY_WORD_DELIMITER = "?";
+
+    function setCategoryWord(){
+        var word =getCategoryWordFromUrl();
+        getCategoryWordSpan().text(word);
+        getCategoryWordInput().val(word);
+    }
+
+    function hasParameter(){
+        return containString(getCurrentUrl(), CATEGORY_WORD_DELIMITER);
+    }
+
+    function getCategoryWordSpan() {
+        return $('span.category_word');
+    }
+    function getCategoryWordInput() {
+        return $('input#category_word');
+    }
+
+    function containString(check_string, target_string) {
+        return check_string.indexOf(target_string) !== -1;
+    }
+
+    function getCurrentUrl() {
+        return window.location.href;
+    }
+
+    function invalidCategoryWord(words){
+        return words.length < 1 || words[1].length < 1;
+    }
+
+    function getCategoryWordFromUrl(){
+        var words = getCurrentUrl().split(CATEGORY_WORD_DELIMITER);
+        if(invalidCategoryWord(words)){
+            return "";
+        }
+        var word = words[1];
+        return decodeURI(word);
+    }
+
+    function decode(str){
+        return decodeURIComponent(escape(atob(str)))
+    }
+
+    function executeMain() {
+        // FIXME: debugging
+//        console.log(encodeURI("Androidアプリ"));
+//        console.log(decode("QW5kcm9pZOOCouODl+ODqg=="));
+        if(!hasParameter()){
+            return;
+        }
+        setCategoryWord();
+    }
+
+    executeMain();
+});
